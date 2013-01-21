6d65bdbed5518588a4a199a0af2c4cba516f701f
Sven Vermeulen <sven.vermeulen@siphos.be>
2013-01-21 22:13:57 +0100

    Adding information on Git and gitolite

--- cfg/puppet/manifests/nodes/pg1.pp
+++ cfg/puppet/manifests/nodes/pg1.pp
@@ -1,6 +1,11 @@
 ##
 ## pg1.internal.genfic.com
 ##
+#
+# PARAMS+
+# MAC=00:11:22:33:44:b4
+# IP6=2001:db8:81::204:de89:3312
+# PARAMS-
 node 'pg1.internal.genfic.com' inherits basenode {
   include general
 }
